31a0dc5c6ab2b42c2b256dbd99edc361a2d894fe
Like Ma <milkpku@gmail.com>
2016-05-14 14:07:30 +0800

    init dataset frame

--- train_policy/dataset.py
+++ train_policy/dataset.py
@@ -4,4 +4,21 @@
 #$Date: Sat May  7 10:59:24 2016
 #$Author: Like Ma <milkpku[at]gmail[dot]com>
 
+Class Dataset(object):
+
+    def __init__(self, datafile_path):
+        pass
+
+
+    def next_batch(self, batch_size):
+        '''
+        return [data, label] with batched size
+        '''
+        pass
+
+def load_data(data_name):
+    '''
+    return dataset which yeild minibatch data
+    '''
+    pass
 
