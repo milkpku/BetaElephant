20860b09ca4c875663b735d88bb18442c497698f
Like Ma <milkpku@gmail.com>
2016-06-13 11:46:49 +0800

    modify export scripts

--- export_net/freeze_graph.sh
+++ export_net/freeze_graph.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ckpt=$1
+outnode=$2
 python3 ~/Documents/BetaElephant/export_net/export_policy.py .
-python3 ~/Documents/tensorflow/tensorflow/python/tools/freeze_graph.py --input_graph=input_graph.pb --input_checkpoint=$ckpt --output_node_names=train-predict --output_graph=output_graph.pb
+python3 ~/Documents/tensorflow/tensorflow/python/tools/freeze_graph.py --input_graph=input_graph.pb --input_checkpoint=$ckpt --output_node_names=$outnode --output_graph=output_graph.pb
