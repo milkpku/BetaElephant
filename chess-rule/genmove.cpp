1d791d9750ac2d4cf7c2e5e1c7b7371bdc639b74
isunchy <sunchyqd@gmail.com>
2016-06-16 00:55:13 +0800

    amend moves generated by genmove()

--- chess-rule/genmove.cpp
+++ chess-rule/genmove.cpp
@@ -5,51 +5,31 @@
 #include <stdlib.h>
 using namespace std;
 
-void rev_str(char *str, int len){
-    char temp;
-    for(int i = 0; i < len/2; i++){
-        temp = str[i];
-        str[i] = str[len - 1 - i];
-        str[len - 1 - i] = temp;
-    }
-}
-
-void rev_letter(char *str, int len){
-    for(int i = 0; i < len; i++){
-        if (isupper(str[i]))
-            str[i] = tolower(str[i]);
-        else if (islower(str[i]))
-            str[i] = toupper(str[i]);
-    }
-}
-
 void turn_round(char *str){
+    
     char *tail = strstr(str, " ");
-    char *start, *end;
-    rev_letter(str, tail - str);
-    start = str;
-    for(int i = 0; i <= 9; i++){
-        if(i == 9)
-            end = strstr(start, " ");
-        else
-            end = strstr(start, "/");
-        rev_str(start, end - start);
-        start = end + 1;
-    }
+    if(*(tail+1)=='b')
+        *(tail+1)='w';
+    else
+        *(tail+1)='b';    
 }
 
 char const* gen(char *infen){
+    char *in = (char *)malloc(1024*sizeof(char));
+    strcpy(in, infen);
     char *out = (char *)malloc(1024*sizeof(char)); 
     char *selfmove, *selfprot, *emymove, *emyprot;
     PreGenInit();
-    selfmove = genMove(infen);
+    selfmove = genMove(in);
     strcat(out, selfmove);
-    selfprot = genProtMove(infen);
-    strcat(out, selfprot);
-    turn_round(infen);
-    emymove = genMove(infen);
+    turn_round(in);
+    emymove = genMove(in);
     strcat(out, emymove);
-    emyprot = genProtMove(infen);
+    turn_round(in);
+    selfprot = genProtMove(in);
+    strcat(out, selfprot);
+    turn_round(in);
+    emyprot = genProtMove(in);
     strcat(out, emyprot);
     return out;
 }
