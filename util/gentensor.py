026140f9d7cc0e090880d58f7f18e8c30c0f163d
isunchy <sunchyqd@gmail.com>
2016-06-16 01:18:18 +0800

    gentensor can work correctly

--- util/gentensor.py
+++ util/gentensor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #-*-coding:utf-8-*-
+#$File: gentensor.py
 
 import pickle
 import copy
@@ -7,10 +8,9 @@ import random
 import numpy as np
 from tensor2fen import tensor2fen
 import genmove
+from dataset import visualdata
 
 OUT_TYPE = np.float32
-chesslayer = {'K':0, 'A':1, 'B':3, 'N':5, 'R':7, 'C':9, 'P':11,
-              'k':0, 'a':1, 'b':3, 'n':5, 'r':7, 'c':9, 'p':11}
 
 
 def gentensor(selfpos, emypos):
@@ -19,52 +19,44 @@ def gentensor(selfpos, emypos):
     return:
         selfmove, emymove, selfprot, selfprot: ndarray [9,10,16]
     '''
-    _fen = tensor2fen(selfpos, emypos)
     fen = tensor2fen(selfpos, emypos)
-    move = genmove.gen(_fen)
+    move = genmove.gen(fen)
     fen = fen + '\t' + move.replace('\n','\t')
-    selfpos, emypos, selfmove, emymove, selfprot, emyprot, movelabel = fen2tensor(fen)
+    selfpos, emypos, selfmove, emymove, selfprot, emyprot = fen2tensor(fen)
     return [selfmove, emymove, selfprot, emyprot]
 
 
 def fen2tensor(fen):
 
-    frdpos = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-    emypos = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-    frdmove = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-    emymove = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-    frdprot = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-    emyprot = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-    movelabel = np.zeros((9, 10, 16), dtype=OUT_TYPE)
-
     fenlist = fen.split('\t')
-    frdpos, emypos = f2tpos(fenlist[0], frdpos, emypos)
-    frdmove = f2tmove(fenlist[1], frdmove, frdpos)
-    emymove = f2tmove(fenlist[3], emymove, emypos)
-    frdprot = f2tmove(fenlist[4], frdprot, frdpos)
-    emyprot = f2tmove(fenlist[5], emyprot, emypos)
+    frdpos, emypos = f2tpos(fenlist[0])
+    frdmove = f2tmove(fenlist[1], frdpos)
+    emymove = f2tmove(fenlist[2], emypos)
+    frdprot = f2tmove(fenlist[3], frdpos)
+    emyprot = f2tmove(fenlist[4], emypos)
 
-    label = fenlist[2].strip().split('-')
-    layer = np.argmax(frdpos[loca2i(label[0][0])][loca2i(label[0][1])])
-    movelabel[loca2i(label[1][0])][loca2i(label[1][1])][layer] = 1
-
-
-    if fenlist[0].split()[1] == 'b':
-        switch_round(frdpos)
-        switch_round(frdmove)
-        switch_round(emypos)
-        switch_round(emymove)
-        switch_round(movelabel)
-        switch_round(frdprot)
-        switch_round(emyprot)
+    turn = fenlist[0].split()[1]
+    if turn == 'b':
+        frdpos = switch_round(frdpos)
+        frdmove = switch_round(frdmove)
+        emypos = switch_round(emypos)
+        emymove = switch_round(emymove)
+        frdprot = switch_round(frdprot)
+        emyprot = switch_round(emyprot)
 
     # shuffle random
-    shuffle([frdpos, frdmove, frdprot, movelabel], shuffle_args())
+    shuffle([frdpos, frdmove, frdprot], shuffle_args())
     shuffle([emypos, emymove, emyprot], shuffle_args())
 
-    return frdpos, emypos, frdmove, emymove, frdprot, emyprot, movelabel
+    #from IPython import embed; embed()
+    return frdpos, emypos, frdmove, emymove, frdprot, emyprot
 
-def f2tpos(fen, frdpos, emypos):
+
+def f2tpos(fen):
+    chesslayer = {'K':0, 'A':1, 'B':3, 'N':5, 'R':7, 'C':9, 'P':11,
+                  'k':0, 'a':1, 'b':3, 'n':5, 'r':7, 'c':9, 'p':11}
+    frdpos = np.zeros((9, 10, 16), dtype=OUT_TYPE)
+    emypos = np.zeros((9, 10, 16), dtype=OUT_TYPE)
     poslist = fen.split()[0].split('/')
     player = fen.split()[1]
     for i in range(len(poslist)):
@@ -89,23 +81,26 @@ def f2tpos(fen, frdpos, emypos):
                 index += int(item[j])
     return frdpos, emypos
 
-def f2tmove(movelist, move, pos):
+def f2tmove(movelist, pos):
+    move = np.zeros((9, 10, 16), dtype=OUT_TYPE)
     movelist = movelist.split()
+    layer = np.argmax(pos, axis=2)
     for item in movelist:
         src = item.split('-')[0]
         des = item.split('-')[1]
-        layer = np.argmax(pos[loca2i(src[0])][loca2i(src[1])])
-        move[loca2i(des[0])][loca2i(des[1])][layer] = 1
+        layerindex = layer[loca2i(src[0])][loca2i(src[1])]
+        move[loca2i(des[0])][loca2i(des[1])][layerindex] = 1
     return move
 
 def loca2i(loc):
     if loc.isupper():
         return ord(loc)-ord('A')
     else:
-        return int(loc)
+        return (9 - int(loc))
 
 def switch_round(mat):
     mat = mat[:,::-1,:]
+    return mat
 
 def shuffle(mat, args):
     index = [[1,2],[3,4],[5,6],[7,8],[9,10],[11,12,13,14,15]]
@@ -133,15 +128,16 @@ def shuffle_args():
 
 
 if __name__ == '__main__':
+
     from dataset import load_data
     dataset = load_data('validation')
     data, label= dataset.next_batch(1000)
 
     for selfpos, emypos, selfmove, emymove, selfprot, emyprot in zip(*data):
-
         new_selfmove, new_emymove, new_selfprot, new_emyprot = gentensor(selfpos, emypos)
-        from IPython import embed; embed()
+        #from IPython import embed; embed()
         assert all( (selfmove.sum(axis=2) == new_selfmove.sum(axis=2)).reshape(-1))
-        assert all( (emymove.sum(axis=2) == new_emymove.sum(axis=2))).reshape(-1)
-        assert all( (selfprot.sum(axis=2) == new_selfport.sum(axis=2))).reshape(-1)
-        assert all( (emyprot.sum(axis=2) == new_emyprot.sum(axis=2))).reshape(-1)
+        assert all( (emymove.sum(axis=2) == new_emymove.sum(axis=2)).reshape(-1))
+        assert all( (selfprot.sum(axis=2) == new_selfprot.sum(axis=2)).reshape(-1))
+        assert all( (emyprot.sum(axis=2) == new_emyprot.sum(axis=2)).reshape(-1))
+
